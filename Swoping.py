1c1616ed02e77cfd32d882b9c4c33047f2670858
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2023-10-31 09:50:50 +0600

    change

--- Swoping.py
+++ Swoping.py
@@ -1,3 +1,4 @@
+"""
 a = 10
 b = 15
 
@@ -7,3 +8,11 @@ a = a-b
 
 # print(" A = ", a," B = ",b)
 print(f"A = {a} B = {b}")  #f_string
+"""
+a = int(input(" Enter the value A : "))
+b = int(input(" Enter the value  B : "))
+
+temp = a
+a = b
+b = temp
+print(" A = ",a, "\n"," B = ",b )
