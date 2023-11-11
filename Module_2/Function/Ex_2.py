045135f5aff0f39ff3deff10beb2cb119bcb743d
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2023-11-11 23:51:52 +0600

    code

--- Module_2/Function/Ex_2.py
+++ Module_2/Function/Ex_2.py
@@ -1,5 +1,5 @@
 def my_function(o, p):
   print(o + " " + p)
 
-my_function("Emil") # its the wrong way to write function
+my_function("Emil") # its the wrong way to send argument function
 my_function("Emil to " ," omit") # its right way
