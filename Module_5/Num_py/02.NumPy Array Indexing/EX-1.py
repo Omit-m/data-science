7ccef72a03504a7d57e9a59dae970db6402528ef
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2023-11-17 00:14:35 +0600

    code

--- Module_5/Num_py/02.NumPy Array Indexing/EX-1.py
+++ Module_5/Num_py/02.NumPy Array Indexing/EX-1.py
@@ -1,4 +1,5 @@
 import  numpy as np
 
 Array = np.array([10,20,30,40,50])
-print(Array[len(Array) - 1]) #last element
+print(Array[0]) # 1st element
+print(Array[len(Array) - 1])  # last element
---
+++ Module_5/Num_py/02.NumPy Array Indexing/Sum-os-arrat-element.py
@@ -0,0 +1,6 @@
+import  numpy as np
+
+Array = np.array([10,20,30,40,50])
+
+print(Array[0] + Array[len(Array)-1])
+print(Array[3] + Array[2]  )
