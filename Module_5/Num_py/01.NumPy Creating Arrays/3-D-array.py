56888a3b8bd35d4ed6e65b1a6efd5a8991d36b93
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2023-11-18 18:04:40 +0600

    code

--- Module_5/Num_py/01.NumPy Creating Arrays/3-D-array.py
+++ Module_5/Num_py/01.NumPy Creating Arrays/3-D-array.py
@@ -1,4 +1,4 @@
 import numpy as np
 array = np.array([[[19,20],[30,27]],[[19,39,],[2662,366]]])
-print(array)
+# print(array)
 print(array.ndim) # Check how many dimensions the arrays have
--- Module_5/Num_py/02.NumPy Array Indexing/Sum-os-array-element.py
+++ Module_5/Num_py/02.NumPy Array Indexing/Sum-os-array-element.py
@@ -4,3 +4,5 @@ Array = np.array([10,20,30,40,50])
 
 print(Array[0] + Array[len(Array)-1])
 print(Array[3] + Array[2]  )
+
+
