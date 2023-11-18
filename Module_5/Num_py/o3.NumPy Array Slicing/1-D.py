24e509d04b0a0b8985e6ed44dd23d7e60d73f7db
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2023-11-18 23:00:16 +0600

    commit

--- Module_5/Num_py/o3.NumPy Array Slicing/1-D.py
+++ Module_5/Num_py/o3.NumPy Array Slicing/1-D.py
@@ -4,9 +4,9 @@ arr=np.array([10,20,30])
 # print(arr[2]) # only one element print
 # print(arr[0:]) # print all element  , [start:End]
 # print(arr[0:3]) # print element [10,20,30]
-print(arr[:2]) # [:End]
+# print(arr[:2]) # [:End]
 
-# print(arr[0:4:2]) # [start:End:step]
+print(arr[0:4:2]) # [start:End:step]
 
 
 
