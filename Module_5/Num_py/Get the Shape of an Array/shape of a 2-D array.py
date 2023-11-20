dd8193884a1705fdd04cb85490667afa5f1fc4ec
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2023-11-21 01:19:02 +0600

    commit

---
+++ Module_5/Num_py/Get the Shape of an Array/shape of a 2-D array.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+'''
+The output will be (2, 4), indicating that arr has 2 rows and 4 columns.
+This is helpful for understanding the structure and dimensions of the 
+array, especially when working with multi-dimensional arrays.
+'''
+
+print(arr.shape)
--- Module_5/Num_py/NumPy Array Copy vs View/VIEW.py
+++ Module_5/Num_py/NumPy Array Copy vs View/VIEW.py
@@ -4,5 +4,5 @@ arr=np.array([10,30,50,60,70])
 x=arr.copy()
 y=arr.view()
 
-print(x.base)
-print(y.base)
+print(x.base) # none becouse (x.base  says that  its  my data)
+print(y.base) # [10,30,50,60,70] (y.base will refer to the original array )
