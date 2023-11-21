f17058962721c99f783167356aeb8a7f1d2035d4
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2023-11-21 16:43:18 +0600

    commit

---
+++ Module_5/Num_py/Searching Arrays/Find the indexes_Ex_2.py
@@ -0,0 +1,18 @@
+# use (np.searchsorted)
+
+
+# import numpy as np
+# arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+# x = np.searchsorted(arr, 8)
+# print(x)
+
+
+
+
+import numpy as np
+
+arr = np.array([6, 7, 8, 9])
+
+x = np.searchsorted(arr, 7)
+
+print(x)
