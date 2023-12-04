24130b61f7e0faad578df76789dd72df83cc67e5
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2023-12-04 22:36:57 +0600

    commit

--- Module_5/Num_py/06.Get the Shape of an Array/5 dimensions using ndmin.py
+++ Module_5/Num_py/06.Get the Shape of an Array/5 dimensions using ndmin.py
@@ -1,5 +1,5 @@
-# import numpy as np
-#
+import numpy as np
+
 # arr = np.array([1, 2, 3, 4,5], ndmin=5)
 #
 # print(arr)
@@ -7,10 +7,14 @@
 #
 
 
-import numpy as np
+# import numpy as np
+#
+# # Generate a single random integer between a specified range (e.g., 0 and 10)
+# random_int = np.random.randint()
+#
+# # Print the random integer
+# print(random_int)
+x = np.random.randint ( 1111 , 9999 )
+print ( x)
 
-# Generate a single random integer between a specified range (e.g., 0 and 10)
-random_int = np.random.randint()
 
-# Print the random integer
-print(random_int)
